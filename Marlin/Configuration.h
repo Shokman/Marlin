f11217f4daa26786d6324555ae433f0361d79afc
Shokman <shokman88@gmail.com>
2016-11-12 19:02:13 -0300

    Select printer model

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -82,11 +82,11 @@
 // 999 = Leapfrog
 
 #ifndef MOTHERBOARD
-#define MOTHERBOARD 7
+#define MOTHERBOARD 33
 #endif
 
 // Define this to set a custom name for your generic Mendel,
-// #define CUSTOM_MENDEL_NAME "This Mendel"
+#define CUSTOM_MENDEL_NAME "P3steel"
 
 // Define this to set a unique identifier for this printer, (Used by some programs to differentiate between machines)
 // You can use an online service to generate a random UUID. (eg http://www.uuidgenerator.net/version4)
