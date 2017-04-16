c73c88fcad4781344c0e33b4c6a71a3ad9e26c55
Shokman <shokman88@gmail.com>
2017-04-15 21:56:57 -0300

    Double steps because we change to DRV8825

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -494,7 +494,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 // default settings
 
 //#define DEFAULT_AXIS_STEPS_PER_UNIT   {78.7402,78.7402,200.0*8/3,760*1.1}  // default steps per unit for Ultimaker
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {80.15, 80.15, 4038, 940.0}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {160.30, 160.30, 8076, 1880.0}
 //#define DEFAULT_MAX_FEEDRATE          {500, 500, 5, 25}    // (mm/sec)
 #define DEFAULT_MAX_FEEDRATE          {500, 500, 2, 45}    // (mm/sec)
 //#define DEFAULT_MAX_ACCELERATION      {9000,9000,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
